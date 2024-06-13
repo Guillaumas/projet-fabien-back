2d1cce5e6c5194b786a6888babc4d6b3702f5cde
Guillaumas <guillaume.gillet.pro@gmail.com>
2024-06-13 17:24:10 +0200

    ça fonctionne pas je veux me coller une balle

--- src/main/java/fr/guigs/api/configs/SecurityConfiguration.java
+++ src/main/java/fr/guigs/api/configs/SecurityConfiguration.java
@@ -1,42 +1,33 @@
 package fr.guigs.api.configs;
 
-import fr.guigs.api.services.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
-    private final UserDetailsServiceImpl userDetailsService;
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
 
-    private final JWTFilter jwtFilter;
+    @Value("${spring.security.oauth2.resourceserver.jwt.audiences}")
+    private String audience;
 
-    private final PasswordEncoder passwordEncoder;
-
-    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService, JWTFilter jwtFilter, PasswordEncoder passwordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.jwtFilter = jwtFilter;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
 
     @Order(Ordered.HIGHEST_PRECEDENCE)
     @Bean
@@ -50,17 +41,30 @@ public class SecurityConfiguration {
                         .requestMatchers(GET, "/v3/api-docs/**").permitAll()
                         .requestMatchers(GET, "/api/assets/**").permitAll()
                         .requestMatchers(GET, "/actuator/**").permitAll()
+                        .requestMatchers(OPTIONS).permitAll()
                         .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer
+                                .jwt(jwt -> {
+                                    jwt.decoder(jwtDecoder());
+                                })
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers("/api/auth/**");
     }
+
 }
+
--- src/main/java/fr/guigs/api/controllers/AuthController.java
+++ src/main/java/fr/guigs/api/controllers/AuthController.java
@@ -1,23 +1,15 @@
 package fr.guigs.api.controllers;
 
 
-import fr.guigs.api.configs.JWTUtil;
-import fr.guigs.api.exceptions.BadCredentialsException;
 import fr.guigs.api.exceptions.UserAlreadyExistsException;
 import fr.guigs.api.models.User;
 import fr.guigs.api.repositories.RoleRepository;
 import fr.guigs.api.repositories.UserRepository;
-import fr.guigs.api.services.TokenValidationService;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -31,17 +23,12 @@ public class AuthController {
 
     private final PasswordEncoder passwordEncoder;
 
-    private final JWTUtil jwtUtil;
 
-    private final TokenValidationService tokenValidationService;
-
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, JWTUtil jwtUtil, TokenValidationService tokenValidationService) {
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
-        this.jwtUtil = jwtUtil;
-        this.tokenValidationService = tokenValidationService;
     }
 
 
@@ -57,38 +44,4 @@ public class AuthController {
         return "User registered successfully";
     }
 
-
-    @PostMapping("/login")
-    public Map<String, String> login(@RequestBody User user) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            String jwt = jwtUtil.generateJwtToken(user.getUsername());
-            Map<String, String> response = new HashMap<>();
-            response.put("token", jwt);
-            return response;
-        } catch (Exception e) {
-            throw new BadCredentialsException("Invalid username or password");
-        }
-    }
-
-    @PostMapping("/checkToken")
-    public Map<String, Boolean> checkToken(@RequestBody Map<String, String> body) {
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("valid", tokenValidationService.isTokenValid(body.get("token")));
-        return response;
-    }
-
-    @PostMapping("/refreshToken")
-    public Map<String, String> refreshToken(@RequestBody Map<String, String> body) {
-        String token = body.get("token");
-        String username = jwtUtil.getUsernameFromJwtToken(token);
-        String newToken = jwtUtil.generateJwtToken(username);
-        Map<String, String> response = new HashMap<>();
-        response.put("token", newToken);
-        return response;
-    }
 }
