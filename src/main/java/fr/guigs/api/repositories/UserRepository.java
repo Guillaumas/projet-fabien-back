ec4dda62385ad5c8d2671695248bd74a01740910
Guillaumas <guillaume.gillet.pro@gmail.com>
2024-06-18 13:46:53 +0200

    ça fonctionne pas je veux me coller une balle

--- src/main/java/fr/guigs/api/repositories/UserRepository.java
+++ src/main/java/fr/guigs/api/repositories/UserRepository.java
@@ -1,12 +1,18 @@
 package fr.guigs.api.repositories;
 
 import fr.guigs.api.models.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     boolean existsByUsername(String username);
+/*
+    Page<User> findAllByOrderByUsernameAsc(Pageable pageable);
+*/
 }
